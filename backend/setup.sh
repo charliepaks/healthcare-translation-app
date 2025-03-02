a837a256789c51c0a8753ac063acced06d7e9ad4
charliepaks <charlesukpaka@ymail.com>
2025-03-02 13:56:15 +0100

    add yaml

--- backend/setup.sh
+++ backend/setup.sh
@@ -4,13 +4,13 @@
 sudo apt-get update && sudo apt-get install -y ffmpeg
 
 # Create Python 3.11 virtual environment
-python3.11 -m venv /app/venv
+python3.11 -m venv venv
 
 # Activate virtual environment
-source /app/venv/bin/activate
+source venv/bin/activate
 
 # Install Python dependencies
-pip install --no-cache-dir -r /app/requirements.txt
+pip install --no-cache-dir -r requirements.txt
 
 # Set environment variables
 export PYTHONUNBUFFERED=1
